80417f84c8ff41ea7a91e9bbb0ed73fd2e511a0a
Ulysse Gérard <thevoodoos@gmail.com>
2026-06-01 14:50:48 +0200

    todo

--- src/index-format/dbllist.ml
+++ src/index-format/dbllist.ml
@@ -87,6 +87,8 @@ let discard t =
       let new_last = l.last.prev in
       (* TODO Should we explicitely disconnect last's pointers ? *)
       new_last.next <- new_last;
+      (* TODO Int.max 0 (l.size - discarded_weight) does seems useless.
+         We could use an assert to check it. *)
       (* Unlinking the discaded cell is not strictly necessary but not doing it
          could lead to memory leaks if the user of the cache keeps a reference
          to the cell. *)
