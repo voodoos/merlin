21ea173876ec0862449924360ccab89210eab48f
Ulysse Gérard <thevoodoos@gmail.com>
2026-06-05 12:48:50 +0200

    dbllist: rfmt

--- src/index-format/dbllist.mli
+++ src/index-format/dbllist.mli
@@ -1,27 +1,18 @@
 type 'a cell =
-  { content : 'a;
-    weight : int;
-    mutable prev : 'a cell;
-    mutable next : 'a cell
-  }
+  { content : 'a; weight : int; mutable prev : 'a cell; mutable next : 'a cell }
 
-type stats = {
-  mutable total_cap : int;
-  mutable promote_count : int;
-  mutable add_count : int;
-  mutable discard_count : int;
-  mutable add_size : int;
-  mutable discarded_size : int;
-}
+type stats =
+  { mutable total_cap : int;
+    mutable promote_count : int;
+    mutable add_count : int;
+    mutable discard_count : int;
+    mutable add_size : int;
+    mutable discarded_size : int
+  }
 
 type 'a dbll =
   | Nil of int
-  | List of
-      { first : 'a cell;
-        last : 'a cell;
-        size : int;
-        cap : int;
-      }
+  | List of { first : 'a cell; last : 'a cell; size : int; cap : int }
 
 type 'a t = { mutable dbll : 'a dbll; stats : stats }
 
